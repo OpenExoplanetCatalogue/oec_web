33a90d2a6b5890b8ce6158a9f994e3d57a098622
Hanno Rein <hanno@hanno-rein.de>
2013-02-06 09:50:02 -0500

    Added optional server side header and footer files

---
+++ .inc/footer.php
@@ -0,0 +1,9 @@
+</div> <!-- content -->
+<div class="clearer">&nbsp;</div>
+<div class="footer">
+The data on this page is taken from the <a href="http://github.com/hannorein/open_exoplanet_catalogue">Open Exoplanet Catalogue</a>. The Open Exoplanet Catalogue as well as this website itself are licensed under an <a href="https://github.com/hannorein/open_exoplanet_catalogue/blob/master/README.md">MIT license</a>.
+</div> <!-- footer -->
+</div> <!-- inner container -->
+</div> <!-- outer container -->
+</body>
+</html>
