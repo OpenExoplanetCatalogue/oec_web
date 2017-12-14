6446cb9ee3fec28088c848eda2df32c393b5e3c0
Andrew Tribick <ajtribick@googlemail.com>
2017-12-14 21:40:55 +0100

    Prefer estimation of luminosity from temperature and radius

--- habitablezone.py
+++ habitablezone.py
@@ -1,59 +1,51 @@
 from numberformat import getFloat, getText
 from math import *
 
+spectralTypeTempRadius = {
+    "O": (40000., 10.),
+    "B": (20000., 3.0),
+    "A": (8500., 1.5),
+    "F": (6500., 1.3),
+    "G": (5500., 1.0),
+    "K": (4000., 0.8),
+    "M": (3000., 0.5)
+}
+
 def hzLimits(xmlPair):
     system, planet, star, filename = xmlPair
     if star is None:
         return None
 
     temperature = getFloat(star,"./temperature")
-    spectralTypeMain = getText(star,"./spectraltype","G")[0]
-    if temperature is None:
-        if spectralTypeMain=="O":
-            temperature = 40000
-        if spectralTypeMain=="B":
-            temperature = 20000
-        if spectralTypeMain=="A":
-            temperature = 8500
-        if spectralTypeMain=="F":
-            temperature = 6500
-        if spectralTypeMain=="G":
-            temperature = 5500
-        if spectralTypeMain=="K":
-            temperature = 4000
-        if spectralTypeMain=="M":
-            temperature = 3000
-        else:
+    stellarRadius = getFloat(star,"./radius")
+    if temperature is not None and stellarRadius is not None:
+        if stellarRadius < 0.01:
+            # no habitable zone for pulsars
             return None
+        luminosity = (temperature/5778.)**4 * stellarRadius**2
+    else:
+        stellarMass = getFloat(star,"./mass")
+        if stellarMass is None:
+            return None
+        elif stellarMass>2.:
+            luminosity = pow(stellarMass,3.5)
+        else:
+            luminosity = pow(stellarMass,4.)
+
+    spectralTypeMain = getText(star,"./spectraltype","")[:1]
+    try:
+        spTemperature, spRadius = spectralTypeTempRadius[spectralTypeMain]
+        if temperature is None:
+            temperature = spTemperature
+        if stellarRadius is None:
+            stellarRadius = spRadius
+    except KeyError:
+        if temperature is None:
+            temperature = 5700.
+        if stellarRadius is None:
+            stellarRadius = 1.
 
     rel_temp = temperature - 5700.
-
-    stellarMass = getFloat(star,"./mass")
-    if stellarMass is None:
-        stellarMass = 1.
-
-    stellarRadius = getFloat(star,"./radius")
-    if stellarRadius is None or stellarRadius<0.01:
-        stellarRadius = 1.
-        if spectralTypeMain=='O':
-            stellarRadius=10.
-        if spectralTypeMain=='B':
-            stellarRadius=3.0
-        if spectralTypeMain=='A':
-            stellarRadius=1.5
-        if spectralTypeMain=='F':
-            stellarRadius=1.3
-        if spectralTypeMain=='G':
-            stellarRadius=1.0
-        if spectralTypeMain=='K':
-            stellarRadius=0.8
-        if spectralTypeMain=='M':
-            stellarRadius=0.5
-
-    if stellarMass>2.:
-        luminosity = pow(stellarMass,3.5)
-    else:
-        luminosity = pow(stellarMass,4.)
 
     # Ref: http://adsabs.harvard.edu/abs/2007A%26A...476.1373S
     HZinner2 = (0.68-2.7619e-5*rel_temp-3.8095e-9*rel_temp*rel_temp) *sqrt(luminosity);
